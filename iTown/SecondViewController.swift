16fa0d28326bcfada9ed4d179040898311886e88
iMigno <mignogna.alessandro@gmail.com>
2016-04-26 10:46:07 +0200

    first commit with gitignore

---
+++ iTown/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iTown
+//
+//  Created by Alessandro on 26/04/16.
+//  Copyright © 2016 Alessandro Mignogna. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
